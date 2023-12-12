b9fdd15e8ea821ccbb33149c4570ff958907cd21
shahid-orion <sroutfit@gmail.com>
2023-12-12 22:20:51 +1100

    fix icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import dropboxLogo from '../../resources/icons8-dropbox.svg'
+import dropboxLogo from '../resources/icons8-dropbox.svg'
 import { SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
 import { ThemeToggler } from './ThemeToggler'
 
@@ -12,13 +12,7 @@ const Header = (props: Props) => {
 		<header className='flex items-center justify-between'>
 			<Link href='/' className='flex items-center space-x-2'>
 				<div className='w-fit'>
-					{/* <Image src={dropboxLogo} alt='Dropbox' width={50} height={50} /> */}
-					<Image
-						src='https://icons8.com/icon/13657/dropbox'
-						alt='Dropbox'
-						width={50}
-						height={50}
-					/>
+					<Image src={dropboxLogo} alt='Dropbox' width={50} height={50} />
 				</div>
 				<h1 className='font-bold text-xl'>Dropbox</h1>
 			</Link>
